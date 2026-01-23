c7298ba32c6388e1e97d5e23a7aab6aaf51aaa59
ray <raychua0106@gmail.com>
2026-01-23 23:02:56 +0800

    Add personality

--- src/main/java/Snorax.java
+++ src/main/java/Snorax.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Snorax {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
         String greetings = "Hello! I'm Snorax";
         String question = "What can I do for you?";
-        String bye = "Bye. Hope to see you again soon!";
+        String bye = "Goodnight. Time for a nap!";
         System.out.println(line + "\n"
                             + greetings + "\n"
                             + question + "\n"
