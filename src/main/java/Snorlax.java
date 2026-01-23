e981eaf341f52425bdd7762a315811b3424c3f4c
ray <raychua0106@gmail.com>
2026-01-23 20:13:41 +0800

    Add greeting, rename main class

---
+++ src/main/java/Snorlax.java
@@ -0,0 +1,14 @@
+public class Snorlax {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String greetings = "Hello! I'm Snorlax";
+        String question = "What can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+        System.out.println(line + "\n"
+                            + greetings + "\n"
+                            + question + "\n"
+                            + line + "\n"
+                            + bye + "\n"
+                            + line + "\n");
+    }
+}
