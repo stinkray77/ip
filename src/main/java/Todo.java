2d58a2bcc561323b247200144c5e221a37f0f97f
ray <raychua0106@gmail.com>
2026-01-26 00:43:54 +0800

    Add TaskType enum and support for TaskType

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -5,6 +5,6 @@ public class Todo extends Task {
 
     @Override
     public String toString() {
-        return "[T]" + super.toString();
+        return "[" +  TaskType.TODO.getSymbol() + "]" + super.toString();
     }
 }
