2fb5daeabeb469d6e2f212a682e9636b9eb33fa7
ray <raychua0106@gmail.com>
2026-02-03 14:38:52 +0800

    Edit Deadline.java to add support for LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,31 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyy h:mma");
 
     public Deadline(String description, String by) {
+        super(description);
+        try {
+            this.by = LocalDateTime.parse(by, INPUT_FORMAT);
+        } catch (DateTimeParseException e) {
+            this.by = LocalDateTime.parse(by);
+        }
+    }
+
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
     public String getBy() {
+        return by.format(INPUT_FORMAT);
+    }
+
+    public LocalDateTime getByDateTime() {
         return by;
     }
     
