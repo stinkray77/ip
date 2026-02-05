fbe9aa2b744f809acb86a52ef760211bf41b36f9
ray <raychua0106@gmail.com>
2026-02-06 02:53:28 +0800

    Comply coding standard for SnoraxException.java

--- src/main/java/snorax/exception/SnoraxException.java
+++ src/main/java/snorax/exception/SnoraxException.java
@@ -1,6 +1,16 @@
 package snorax.exception;
 
+/**
+ * Represents an exception specific to the Snorax application.
+ * Used to signal errors that occur during Snorax operations.
+ */
 public class SnoraxException extends Exception {
+
+    /**
+     * Constructs a new SnoraxException with the specified error message.
+     *
+     * @param message The error message describing what went wrong.
+     */
     public SnoraxException(String message) {
         super(message);
     }
