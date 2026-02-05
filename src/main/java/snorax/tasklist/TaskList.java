6e8ad25a3c06955ab04ed4eb28afdda80190973a
ray <raychua0106@gmail.com>
2026-02-06 02:46:56 +0800

    Comply coding standard for TaskList.java

--- src/main/java/snorax/tasklist/TaskList.java
+++ src/main/java/snorax/tasklist/TaskList.java
@@ -3,33 +3,72 @@ package snorax.tasklist;
 import java.util.ArrayList;
 import snorax.task.Task;
 
+/**
+ * Represents a list of tasks.
+ * Provides operations to add, delete, and retrieve tasks.
+ */
 public class TaskList {
     private ArrayList<Task> tasks;
 
+    /**
+     * Constructs an empty TaskList.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Constructs a TaskList with the given list of tasks.
+     *
+     * @param tasks The initial list of tasks.
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task The task to be added.
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Deletes a task at the specified index.
+     *
+     * @param index The index of the task to delete.
+     * @return The deleted task.
+     */
     public Task deleteTask(int index) {
         return tasks.remove(index);
     }
 
+    /**
+     * Retrieves a task at the specified index.
+     *
+     * @param index The index of the task to retrieve.
+     * @return The task at the specified index.
+     */
     public Task getTask(int index) {
         return tasks.get(index);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return The size of the task list.
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Returns the internal list of tasks.
+     *
+     * @return The ArrayList containing all tasks.
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
