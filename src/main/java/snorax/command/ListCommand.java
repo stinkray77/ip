ecc6585b9c830b60fbb053a5a6d278fb8f984ee7
ray <raychua0106@gmail.com>
2026-02-06 02:55:53 +0800

    Comply coding standard for ListCommand.java

--- src/main/java/snorax/command/ListCommand.java
+++ src/main/java/snorax/command/ListCommand.java
@@ -4,12 +4,28 @@ import snorax.storage.Storage;
 import snorax.tasklist.TaskList;
 import snorax.ui.Ui;
 
+/**
+ * Represents a command to list all tasks.
+ */
 public class ListCommand extends Command {
+
+    /**
+     * Executes the list command by displaying all tasks.
+     *
+     * @param tasks   The task list containing all tasks.
+     * @param ui      The user interface for displaying messages.
+     * @param storage The storage handler for saving tasks.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.showTaskList(tasks);
     }
 
+    /**
+     * Indicates whether this command will exit the application.
+     *
+     * @return false, as this command does not exit the application.
+     */
     @Override
     public boolean isExit() {
         return false;
