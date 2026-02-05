8752ac64d2fb5ab323c74c358d9de8349fbd181c
ray <raychua0106@gmail.com>
2026-02-05 19:50:36 +0800

    Change Command.java to abstract class

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,40 +1,14 @@
-public enum Command {
-    BYE,
-    LIST,
-    MARK,
-    UNMARK,
-    DELETE,
-    TODO,
-    DEADLINE,
-    EVENT,
-    UNKNOWN;
+package commands;
 
-    public static Command parse(String input) {
-        if (input == null || input.trim().isEmpty()) {
-            return UNKNOWN;
-        }
-        
-        String command = input.split(" ")[0].toLowerCase();
-        
-        switch (command) {
-            case "bye":
-                return BYE;
-            case "list":
-                return LIST;
-            case "mark":
-                return MARK;
-            case "unmark":
-                return UNMARK;
-            case "delete":
-                return DELETE;
-            case "todo":
-                return TODO;
-            case "deadline":
-                return DEADLINE;
-            case "event":
-                return EVENT;
-            default:
-                return UNKNOWN;
-        }
-    }
+import Storage;
+import TaskList;
+import Ui;
+import SnoraxException;
+
+public abstract class Command {
+    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws SnoraxException;
+
+    public abstract boolean isExit();
 }
+
+
