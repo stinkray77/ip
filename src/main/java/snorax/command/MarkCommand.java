1554a8721b59d113c7c27fc270e015846e471519
ray <raychua0106@gmail.com>
2026-02-06 02:55:11 +0800

    Comply coding standard for MarkCommand.java

--- src/main/java/snorax/command/MarkCommand.java
+++ src/main/java/snorax/command/MarkCommand.java
@@ -6,13 +6,29 @@ import snorax.task.Task;
 import snorax.tasklist.TaskList;
 import snorax.ui.Ui;
 
+/**
+ * Represents a command to mark a task as done.
+ */
 public class MarkCommand extends Command {
     private int index;
 
+    /**
+     * Constructs a MarkCommand with the specified task index.
+     *
+     * @param index The index of the task to mark (0-based).
+     */
     public MarkCommand(int index) {
         this.index = index;
     }
 
+    /**
+     * Executes the mark command by marking the specified task as done.
+     *
+     * @param tasks   The task list containing all tasks.
+     * @param ui      The user interface for displaying messages.
+     * @param storage The storage handler for saving tasks.
+     * @throws SnoraxException If the task index is invalid.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws SnoraxException {
         Task task = tasks.getTask(index);
@@ -21,6 +37,11 @@ public class MarkCommand extends Command {
         ui.showTaskMarked(task);
     }
 
+    /**
+     * Indicates whether this command will exit the application.
+     *
+     * @return false, as this command does not exit the application.
+     */
     @Override
     public boolean isExit() {
         return false;
