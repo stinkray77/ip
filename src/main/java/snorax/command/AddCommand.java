07d2867733da729b27b8003e19f0212de4f112b5
ray <raychua0106@gmail.com>
2026-02-06 02:44:50 +0800

    Comply coding standard for AddCommand.java

--- src/main/java/snorax/command/AddCommand.java
+++ src/main/java/snorax/command/AddCommand.java
@@ -6,20 +6,42 @@ import snorax.task.Task;
 import snorax.tasklist.TaskList;
 import snorax.ui.Ui;
 
+/**
+ * Represents a command to add a task to the task list.
+ */
 public class AddCommand extends Command {
     private Task task;
 
+    /**
+     * Constructs an AddCommand with the specified task.
+     *
+     * @param task The task to be added.
+     */
     public AddCommand(Task task) {
         this.task = task;
     }
 
+    /**
+     * Executes the add command by adding the task to the task list,
+     * saving to storage, and displaying confirmation to the user.
+     *
+     * @param tasks   The task list to add the task to.
+     * @param ui      The UI to display messages.
+     * @param storage The storage to save the updated task list.
+     * @throws SnoraxException If there is an error saving to storage.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws SnoraxException {
         tasks.addTask(task);
         storage.save(tasks.getTasks());
-        ui.showTaskAdded(task, tasks.size());
+        ui.showTaskAdded(task.toString(), tasks.size());
     }
 
+    /**
+     * Indicates whether this command will cause the application to exit.
+     *
+     * @return false, as this command does not exit the application.
+     */
     @Override
     public boolean isExit() {
         return false;
