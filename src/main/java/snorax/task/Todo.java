787354468cc0028e4b3904692c90898ca9dcf033
ray <raychua0106@gmail.com>
2026-02-06 02:47:27 +0800

    Comply coding standard for Todo.java

--- src/main/java/snorax/task/Todo.java
+++ src/main/java/snorax/task/Todo.java
@@ -1,12 +1,26 @@
 package snorax.task;
 
+/**
+ * Represents a todo task without any date/time attached to it.
+ */
 public class Todo extends Task {
+
+    /**
+     * Constructs a Todo task with the specified description.
+     *
+     * @param description The description of the todo task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns a string representation of the todo task.
+     *
+     * @return String with format "[T][status] description".
+     */
     @Override
     public String toString() {
-        return "[" +  TaskType.TODO.getSymbol() + "]" + super.toString();
+        return "[" + TaskType.TODO.getSymbol() + "]" + super.toString();
     }
 }
