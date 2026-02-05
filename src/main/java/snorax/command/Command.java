fd2249e794b1469a2951e66e8d8696ae6f243ced
ray <raychua0106@gmail.com>
2026-02-06 02:18:10 +0800

    Add JavaDoc comments for Command.java

--- src/main/java/snorax/command/Command.java
+++ src/main/java/snorax/command/Command.java
@@ -5,9 +5,27 @@ import snorax.storage.Storage;
 import snorax.tasklist.TaskList;
 import snorax.ui.Ui;
 
+/**
+ * Represents an executable command in the Snorax application.
+ * This is an abstract class that all specific command types extend.
+ */
 public abstract class Command {
+    
+    /**
+     * Executes the command with the given task list, UI, and storage.
+     *
+     * @param tasks The task list to operate on.
+     * @param ui The UI to interact with the user.
+     * @param storage The storage to save or load tasks.
+     * @throws SnoraxException if there is an error during execution.
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws SnoraxException;
 
+    /**
+     * Indicates whether this command causes the application to exit.
+     *
+     * @return true if the application should exit after this command, false otherwise.
+     */
     public abstract boolean isExit();
 }
 
