531ff4a4d3da485dbe50e887e6117213b35a1008
ray <raychua0106@gmail.com>
2026-02-06 02:56:24 +0800

    Comply coding standard for ExitCommand.java

--- src/main/java/snorax/command/ExitCommand.java
+++ src/main/java/snorax/command/ExitCommand.java
@@ -4,13 +4,29 @@ import snorax.storage.Storage;
 import snorax.tasklist.TaskList;
 import snorax.ui.Ui;
 
+/**
+ * Represents a command to exit the application.
+ */
 public class ExitCommand extends Command {
+
+    /**
+     * Executes the exit command by displaying a goodbye message and closing the UI.
+     *
+     * @param tasks   The task list containing all tasks.
+     * @param ui      The user interface for displaying messages.
+     * @param storage The storage handler for saving tasks.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.showGoodbye();
         ui.close();
     }
 
+    /**
+     * Indicates whether this command will exit the application.
+     *
+     * @return true, as this command exits the application.
+     */
     @Override
     public boolean isExit() {
         return true;
