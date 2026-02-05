7b5fb2252a0bc63731ab798c1fb2480a0dc3f1b7
ray <raychua0106@gmail.com>
2026-02-06 02:54:22 +0800

    Comply coding standard for UnmarkCommand.java

--- src/main/java/snorax/command/UnmarkCommand.java
+++ src/main/java/snorax/command/UnmarkCommand.java
@@ -6,13 +6,29 @@ import snorax.task.Task;
 import snorax.tasklist.TaskList;
 import snorax.ui.Ui;
 
+/**
+ * Represents a command to unmark a task as not done.
+ */
 public class UnmarkCommand extends Command {
     private int index;
 
+    /**
+     * Constructs an UnmarkCommand with the specified task index.
+     *
+     * @param index The index of the task to unmark (0-based).
+     */
     public UnmarkCommand(int index) {
         this.index = index;
     }
 
+    /**
+     * Executes the unmark command by marking the specified task as not done.
+     *
+     * @param tasks   The task list containing all tasks.
+     * @param ui      The user interface for displaying messages.
+     * @param storage The storage handler for saving tasks.
+     * @throws SnoraxException If the task index is invalid.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws SnoraxException {
         Task task = tasks.getTask(index);
@@ -21,6 +37,11 @@ public class UnmarkCommand extends Command {
         ui.showTaskUnmarked(task);
     }
 
+    /**
+     * Indicates whether this command will exit the application.
+     *
+     * @return false, as this command does not exit the application.
+     */
     @Override
     public boolean isExit() {
         return false;
